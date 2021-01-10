d2c43984b8b3d930cb367d2075991df327914346
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2021-01-10 20:03:51 +0100

    Enable Vue with little example component

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,12 +3,16 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## unreleased
+## ## 2021-01-10
+
 
 ### Added
 
+- Upgrade Vue to Vue 3.03 (was not 3.03 yet...)
+- Enable Vue with the one example component (=> show it in home)
+- Adapt gitignore 
 
-## 2021-01-10
+## 2021-01-09
 
 ### Added
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,9 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+//require('./bootstrap');
 
-window.Vue = require('vue').default;
+//window.Vue = require('vue').default;
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +19,7 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,7 +27,32 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app',
-});
-/* @todo: Rework */
+//const app = new Vue({
+//    el: '#app',
+//});
+
+/* @todo: Rework Register automatically */
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+// Load vue 
+window.Vue = require('vue');
+
+// Create the app (without any parameters }
+const app = Vue.createApp({});
+
+// Import the component
+import ExampleComponent from './components/ExampleComponent.vue'
+
+// Register the component
+app.component('example-component', ExampleComponent);
+
+// Mount the app 
+app.mount('#app');
+
